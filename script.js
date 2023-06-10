28edbc8cb96b7c83a3bca0c9deda44695b63c413
ihor97 <daniel.waffner@gmail.com>
2023-06-10 11:47:01 +0100

    деякі розяснення

--- script.js
+++ script.js
@@ -1,16 +1,7 @@
+/*
+у Function Object Array властивості __proto__ і prototype співпадають
+Function.prototype -native code
 
-// Object.create(obj) створює обєкт який буде зсилатися на obj
 
-// const person={
-//     isHuman:true,
-//     print:function(){
-//         console.log(`My name is ${this.name}. Am i human ${this.isHuman}`);
-//     }
-// }
-// const me=Object.create(person)
 
-// me.name='haha'
-
-// me.print()
-
-// різниця між Object.create() i Object.setPrototypeOf() в тому що Object.create() створює новий обєкт 
+*/ 
