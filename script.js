c1e73af282fbe52c4636270a991e147b9b3ec9db
ihor97 <daniel.waffner@gmail.com>
2023-06-13 19:29:52 +0100

    приклад з завантаженням даних

--- script.js
+++ script.js
@@ -1,7 +1,25 @@
-// викине через 4000 тому що перший колбек викинеться через 1000 тільки
-setTimeout(() => {
-    console.log(1);
-    setTimeout(() => {
-        console.log(2);
-    }, 3000);
-}, 1000);
+const btn=document.querySelector('.btn')
+const wrapper=document.querySelector('.wrapper')
+const line=document.querySelector('.line')
+const fetchFromserver=(arr,count)=>{
+let counter=1
+const id=setInterval(() => {
+    arr.push(`File ${counter}`)
+    counter++
+    if(counter>count){
+        clearInterval(id)
+    }
+}, 700);
+}
+const download=(count)=>{
+    const result=[]
+    fetchFromserver(result,count)
+   const id= setInterval(() => {
+    // під час виконання цього інтервалу ми можемо взаємодіяти зі сторінкою без проблем
+        line.style.width=result.length*(300/count)+'px'
+        if(result.length===count){
+            clearInterval(id)
+        }
+    }, 100);
+}
+btn.onclick=()=>download(6)
