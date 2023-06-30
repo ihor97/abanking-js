287fbe1f8ce35eabe2e95ed953b1f7ff4be28bb1
ihor97 <daniel.waffner@gmail.com>
2023-06-30 21:24:59 +0100

    [Symbol.iterator]: задавання ф-ї

--- script.js
+++ script.js
@@ -1,9 +1,43 @@
-const fs = require('fs/promises')
-
-fs.writeFile('./text.txt','hello').then(
-    ()=>{
-        console.log('res');
+// приклад 
+const iterableObject = {
+    data: [1, 2, 3],
+    [Symbol.iterator]: function () {
+      let index = 0;
+      const data = this.data;
+      
+      return {
+        next: function () {
+          if (index < data.length) {
+            return { value: data[index++], done: false };
+          } else {
+            // якщо value  не вказати то воно і так буде undefined
+            return { done: true };
+          }
+        }
+      };
     }
-)
+  };
+  
+  for (const item of iterableObject) {
+    console.log(item);
+  }
 
-console.log('dsdsds');
+class CountDown{
+    constructor(start){
+        this.nextValue=start
+    }
+    // цей символ є для того щоб ми не могли створити свій ітератор і щоб не перезаписалися інші ітератори тобто воно є унікальним
+    [Symbol.iterator](){
+        let nextValue=this.nextValue
+        return ()=>{
+            return {
+
+                next(){}
+            }
+        }
+    }
+}
+const x = new CountDown()
+for (const iterator of x) {
+    
+}
