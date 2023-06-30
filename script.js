df9e7ce55d07ab2accceb4df06f701d3c73e248d
ihor97 <daniel.waffner@gmail.com>
2023-06-30 20:58:20 +0100

    нода

--- script.js
+++ script.js
@@ -1,15 +1,9 @@
-function *foo(x) {
-    // yield може як приймати так і віддавати
-    const y=x *(yield 'please input number')
-    return y
-}
-// це можна заюзати якщо у нас є якісь тяжкі обчислення і нам треба дочекатися значення якогось яке прийде пізніше
-// так що ф-я може зупинитися на якомусь моменті і чекати введення цього значення
-const iterator=foo(5)
-// iterator.next()
-console.log(iterator.next());
+const fs = require('fs/promises')
 
-// ми тут кажемо замість yield підставити 2
-console.log(iterator.next(2));
-// console.log(iterator.next());
+fs.writeFile('./text.txt','hello').then(
+    ()=>{
+        console.log('res');
+    }
+)
 
+console.log('dsdsds');
